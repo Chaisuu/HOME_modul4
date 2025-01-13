a6653730a9131872c448a6ae82d9e2f2881f566c
Chaisuu <Kfire13@yandex.ru>
2025-01-14 00:07:28 +0700

    код проверен flake8

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,6 +1,7 @@
 def test_category_init(category):
     assert category.name == "Смартфоны"
-    assert category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert category.description == ("Смартфоны, как средство не только коммуникации, "
+                                    "но и получения дополнительных функций для удобства жизни")
     assert category.products == ["product1", "product2", "product3"]
     assert category.count_of_categories == 1
     assert category.count_of_products == 3
